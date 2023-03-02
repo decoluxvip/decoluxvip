e08f016c49686676ec6e0761b3c65c00ccec3f16
decoluxvip <samad.samadov@hotmail.com>
2023-03-02 16:08:54 +0400

    ilk proyektim templates

--- ilkproject/blog/urls.py
+++ ilkproject/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.index),
     path("index", views.index),
     path("blogs", views.blogs),
-    path("blogs/<int:id>", views.blog_details),
+    path("blog-details/<int:id>", views.blog_details),
 ]
--- ilkproject/ilkproject/settings.py
+++ ilkproject/ilkproject/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'ilkproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "Templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
