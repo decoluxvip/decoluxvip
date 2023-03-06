8dddd3634eb133cf20d4a27493feee56e91dfb9d
decoluxvip <samad.samadov@hotmail.com>
2023-03-06 16:33:34 +0400

    linklər

--- ilkproject/blog/urls.py
+++ ilkproject/blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="home"),
     path("index", views.index),
-    path("blogs", views.blogs),
-    path("blog-details/<int:id>", views.blog_details),
+    path("blogs", views.blogs, name="blogs"),
+    path("contact", views.contact, name="contact"),
+    path("about", views.about, name="about"),
+    path("blog-details/<int:id>", views.blog_details, name="blog-details"),
 ]
--- ilkproject/blog/views.py
+++ ilkproject/blog/views.py
@@ -1,21 +1,70 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "Nağıllar",
+            "image": "1.png",
+            "is_active": True,
+            "is_home": True,
+            "description": "Azərbaycan nağılları"
+        },
+        {
+            "id": 2,
+            "title": "Bədii ədəbiyyat",
+            "image": "2.png",
+            "is_active": True,
+            "is_home": True,
+            "description": "Bədii ədəbiyyat"
+        },
+        {
+            "id": 3,
+            "title": "Xarici ədəbiyyat",
+            "image": "3.png",
+            "is_active": True,
+            "is_home": False,
+            "description": "Xarici"
+        },
+        {
+            "id": 4,
+            "title": "Dedektiv",
+            "image": "4.png",
+            "is_active": False,
+            "is_home": True,
+            "description": "Dedektiv janr"
+        },
+    ]
+}
+
 
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html")
+
+
+def contact(request):
+    return render(request, "blog/contact.html")
 
 
 def blog_details(request, id):
-    if id < 4:
-        z = "salam: "
-    else:
-        z = "sagol: "
+    blogs = data["blogs"]
+    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
     return render(request, "blog/blog-details.html", {
-        "id": z+str(id)
+        "blog": selectedBlog
     })
